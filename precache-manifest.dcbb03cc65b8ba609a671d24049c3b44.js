d173484dfd4c607d88407b816f57ca20f583a659
Ian Cumming <32406158+IanMarcC@users.noreply.github.com>
2019-09-20 15:55:06 -0700

    more files for testing

---
+++ precache-manifest.dcbb03cc65b8ba609a671d24049c3b44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae7bceb78da5388580a25815e561dad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa3a7324a83050952b52",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5ffe8627f4257e4395a0",
+    "url": "/static/js/2.376f8d6f.chunk.js"
+  },
+  {
+    "revision": "aa3a7324a83050952b52",
+    "url": "/static/js/main.67f2d4f5.chunk.js"
+  },
+  {
+    "revision": "cb4fd463e7184abd8f6a",
+    "url": "/static/js/runtime~main.d52357dd.js"
+  }
+]);
